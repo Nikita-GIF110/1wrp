605c3cfef737df2d66d23d27b5dbe043545fc279
Nikita-GIF110 <nikita-ivanov97@bk.ru>
2024-06-30 00:26:40 +0200

    dev

--- frontend/src/features/news/ui/shared-links.tsx
+++ frontend/src/features/news/ui/shared-links.tsx
@@ -4,7 +4,7 @@ import { colors } from "shared/config/colors";
 import { CopyButton } from "shared/ui/copy-button";
 import LinkChain from "assets/images/home/link-chain-icon.svg?react";
 import DiscordSmall from "assets/icons/discord-small.svg?react";
-import VkSmall from "assets/icons/vk-small.svg?react";
+import Vk from "assets/icons/vk.svg?react";
 import YouTubeSmall from "assets/icons/youtube-small.svg?react";
 
 interface SharedLinksProps extends Omit<BoxProps, "children"> {}
@@ -37,7 +37,7 @@ export const SharedLinks = (boxProps: SharedLinksProps) => (
       </CopyButton>
 
       <Button variant="circleLight" backgroundColor="#BEC7CD">
-        <Box as={VkSmall} width="24px" height="24px" />
+        <Box as={Vk} width="24px" height="24px" />
       </Button>
       <Button variant="circleLight" backgroundColor="#BEC7CD">
         <Box as={DiscordSmall} width="24px" height="24px" />
